8ceb3d582ce13fd0a49977bf0d5b0b207ce0dd68
小yeah <aidyfeng@qq.com>
2024-11-13 18:01:33 +0800

    提交代码

---
+++ src/error.rs
@@ -0,0 +1,6 @@
+//自定义result 类型
+pub type Result<T> = std::result::Result<T,Error>;
+
+pub enum Error{
+    
+}
---
+++ src/sql/parser/lexer.rs
@@ -0,0 +1,68 @@
+use std::{iter::Peekable, str::Chars};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    // 关键字
+    Keyword(Keyword),
+    // 其他类型的字符串Token，比如表名、列名
+    Ident(String),
+    // 字符串类型的数据
+    String(String),
+    // 数值类型，比如整数和浮点数
+    Number(String),
+    // 左括号 (
+    OpenParen,
+    // 右括号 )
+    CloseParen,
+    // 逗号 ,
+    Comma,
+    // 分号 ;
+    Semicolon,
+    // 星号 *
+    Asterisk,
+    // 加号 +
+    Plus,
+    // 减号 -
+    Minus,
+    // 斜杠 /
+    Slash,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Keyword {
+    Create,
+    Table,
+    Int,
+    Integer,
+    Boolean,
+    Bool,
+    String,
+    Text,
+    Varchar,
+    Float,
+    Double,
+    Select,
+    From,
+    Insert,
+    Into,
+    Values,
+    True,
+    False,
+    Default,
+    Not,
+    Null,
+    Primary,
+    Key,
+}
+
+pub struct Lexer<'a> {
+    iter: Peekable<Chars<'a>>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(sql_test: &'a str) -> Self {
+        Self {
+            iter: sql_test.chars().peekable()
+        }
+    }
+}
