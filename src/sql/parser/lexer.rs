8d7ed6e47985242e2d7e81b91f0602c720b3390c
小yeah <aidyfeng@qq.com>
2024-11-16 21:20:50 +0800

    update

--- src/sql/parser/lexer.rs
+++ src/sql/parser/lexer.rs
@@ -57,8 +57,85 @@ pub enum Keyword {
     Key,
 }
 
+impl Keyword {
+    pub fn from_str(ident: &str) -> Option<Self> {
+        Some(match ident.to_uppercase().as_ref() {
+            "CREATE" => Keyword::Create,
+            "TABLE" => Keyword::Table,
+            "INT" => Keyword::Int,
+            "INTEGER" => Keyword::Integer,
+            "BOOLEAN" => Keyword::Boolean,
+            "BOOL" => Keyword::Bool,
+            "STRING" => Keyword::String,
+            "TEXT" => Keyword::Text,
+            "VARCHAR" => Keyword::Varchar,
+            "FLOAT" => Keyword::Float,
+            "DOUBLE" => Keyword::Double,
+            "SELECT" => Keyword::Select,
+            "FROM" => Keyword::From,
+            "INSERT" => Keyword::Insert,
+            "INTO" => Keyword::Into,
+            "VALUES" => Keyword::Values,
+            "TRUE" => Keyword::True,
+            "FALSE" => Keyword::False,
+            "DEFAULT" => Keyword::Default,
+            "NOT" => Keyword::Not,
+            "NULL" => Keyword::Null,
+            "PRIMARY" => Keyword::Primary,
+            "KEY" => Keyword::Key,
+            _ => return None,
+        })
+    }
+
+    pub fn to_str(&self) -> &str {
+        match self {
+            Keyword::Create => "CREATE",
+            Keyword::Table => "TABLE",
+            Keyword::Int => "INT",
+            Keyword::Integer => "INTEGER",
+            Keyword::Boolean => "BOOLEAN",
+            Keyword::Bool => "BOOL",
+            Keyword::String => "STRING",
+            Keyword::Text => "TEXT",
+            Keyword::Varchar => "VARCHAR",
+            Keyword::Float => "FLOAT",
+            Keyword::Double => "DOUBLE",
+            Keyword::Select => "SELECT",
+            Keyword::From => "FROM",
+            Keyword::Insert => "INSERT",
+            Keyword::Into => "INTO",
+            Keyword::Values => "VALUES",
+            Keyword::True => "TRUE",
+            Keyword::False => "FALSE",
+            Keyword::Default => "DEFAULT",
+            Keyword::Not => "NOT",
+            Keyword::Null => "NULL",
+            Keyword::Primary => "PRIMARY",
+            Keyword::Key => "KEY",
+        }
+    }
+}
+
 pub struct Lexer<'a> {
     iter: Peekable<Chars<'a>>,
+}
+
+/**
+ * 自定义迭代器
+ */
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Result<Token>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.scan() {
+            Ok(Some(token)) => Some(Ok(token)),
+            Ok(None) => self
+                .iter
+                .peek()
+                .map(|it| Err(Error::Parse(format!("[Lexer] unexpected character {}", it)))),
+            Err(err) => Some(Err(err)),
+        }
+    }
 }
 
 impl<'a> Lexer<'a> {
@@ -95,18 +172,27 @@ impl<'a> Lexer<'a> {
     }
 
     /**
+     * 只有token类型,才能跳转到下一个转移
+     */
+    fn next_if_token<F: Fn(char) -> Option<Token>>(&mut self, predicate: F) -> Option<Token> {
+        let token = self.iter.peek().and_then(|&it| predicate(it))?;
+        self.iter.next();
+        Some(token)
+    }
+
+    /**
      * 扫描拿到第一个token
      */
     fn scan(&mut self) -> Result<Option<Token>> {
         //消除字符串中的空白字符
         self.erase_whitespace();
         match self.iter.peek() {
-            Some('\'') => self.scan_string(), //扫描字符串
+            Some('\'') => self.scan_string(),                     //扫描字符串
             Some(c) if c.is_ascii_digit() => Ok(self.scan_num()), // 扫描数字
-            _ => todo!(),
+            Some(c) if c.is_ascii_alphabetic() => Ok(self.scan_ident()), // 扫描字符
+            Some(_) => Ok(self.scan_symbol()),                    // 扫描符号
             None => Ok(None),
-        };
-        todo!()
+        }
     }
 
     /**
@@ -122,7 +208,7 @@ impl<'a> Lexer<'a> {
             match self.iter.next() {
                 Some('\'') => break,
                 Some(c) => value.push(c),
-                None => return Err(Error::Parse(format!("[Lexer] unexpected end of string")))
+                None => return Err(Error::Parse(format!("[Lexer] unexpected end of string"))),
             }
         }
 
@@ -132,18 +218,47 @@ impl<'a> Lexer<'a> {
     /**
      * 扫描数字
      */
-    fn scan_num(&mut self) -> Option<Token>{
+    fn scan_num(&mut self) -> Option<Token> {
         //获取数字
         let mut num = self.next_while(|it| it.is_ascii_digit())?;
 
         //判断是否有小数点, 如果有小数点, 则是浮点数, 继续扫描
-        if let Some(sep) = self.next_if(|it| it == '.'){
+        if let Some(sep) = self.next_if(|it| it == '.') {
             num.push(sep);
-            while let Some(c) = self.next_if(|it| it.is_ascii_digit()){
+            while let Some(c) = self.next_if(|it| it.is_ascii_digit()) {
                 num.push(c);
             }
         }
 
         Some(Token::Number(num))
+    }
+
+    /**
+     * 扫描Ident字符, 例如表名,列名, 也可能是关键字
+     */
+    fn scan_ident(&mut self) -> Option<Token> {
+        let mut value = self.next_if(|it| it.is_alphanumeric())?.to_string();
+
+        while let Some(c) = self.next_if(|it| it.is_alphanumeric() || it == '_') {
+            value.push(c);
+        }
+
+        Some(Keyword::from_str(&value).map_or_else(|| Token::Ident(value), Token::Keyword))
+    }
+
+    fn scan_symbol(&mut self) -> Option<Token> {
+        self.next_if_token(|it| {
+            Some(match it {
+                '*' => Token::Asterisk,
+                '(' => Token::OpenParen,
+                ')' => Token::CloseParen,
+                ',' => Token::Comma,
+                ';' => Token::Semicolon,
+                '+' => Token::Plus,
+                '-' => Token::Minus,
+                '/' => Token::Slash,
+                _ => return None,
+            })
+        })
     }
 }
