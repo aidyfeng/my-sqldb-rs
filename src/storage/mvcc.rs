e3eab3b557c30602552d78788c89ac0adc2d83eb
小yeah <aidyfeng@qq.com>
2024-12-17 23:15:56 +0800

    mvcc开发

--- src/storage/mvcc.rs
+++ src/storage/mvcc.rs
@@ -1,8 +1,10 @@
 use std::sync::{Arc, Mutex};
-
+use serde::{Deserialize, Serialize};
 use crate::error::Result;
 
 use super::engine::Engine;
+
+pub type Version = u64;
 
 pub struct Mvcc<E: Engine> {
     engine: Arc<Mutex<E>>,
@@ -24,7 +26,7 @@ impl<E: Engine> Mvcc<E> {
     }
 
     pub fn begin(&self) -> Result<MvccTransaction<E>> {
-        Ok(MvccTransaction::begin(self.engine.clone()))
+        MvccTransaction::begin(self.engine.clone())
     }
 }
 
@@ -32,9 +34,41 @@ pub struct MvccTransaction<E: Engine> {
     engine: Arc<Mutex<E>>,
 }
 
+#[derive(Debug,Serialize,Deserialize)]
+pub enum MvccKey{
+    NextVersion,
+    TxnActive(Version)
+}
+
+impl MvccKey{
+    pub fn encode(&self) -> Vec<u8>{
+        bincode::serialize(&self).unwrap()
+    }
+
+    pub fn decode(data: Vec<u8>) -> Result<MvccKey> {
+        Ok(bincode::deserialize(&data)?)
+    }
+}
+
 impl<E: Engine> MvccTransaction<E> {
-    pub fn begin(eng: Arc<Mutex<E>>) -> Self {
-        Self { engine: eng }
+    pub fn begin(eng: Arc<Mutex<E>>) -> Result<Self> {
+        //获取存储引擎
+        let mut engine = eng.lock()?;
+        //获取版本号
+        let next_version = match engine.get(MvccKey::NextVersion.encode())? {
+            Some(val) => bincode::deserialize(&val)?,
+            None => 1,
+        };
+
+        engine.set(MvccKey::NextVersion.encode(),bincode::serialize(&(next_version + 1))?)?;
+
+        //获取当前活跃的事务列表
+
+        //当前事务加入到活跃事务列表
+        engine.set(MvccKey::TxnActive(next_version).encode(),vec![])?;
+
+        todo!()
+
     }
 
     pub fn commit(&self) -> Result<()> {
